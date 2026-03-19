8dda939871c9a8f85beef2a35124e4967c1a9d00
Davit Nakhutsrishvili <dtnakhutsrishvili@gmail.com>
2026-03-19 18:33:49 +0400

    feat: implement carousel

--- frontend/src/components/TaskRender.js
+++ frontend/src/components/TaskRender.js
@@ -1,6 +1,11 @@
+import {  useRef ,useState,useEffect} from "react"
 import Checkbox from "./Checkbox"
+import styles from './taskRender.module.css'
 
 function TaskRender({tasks ,onToggleChange,getSelectedTask,getId}) {
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const itemWidth = 300 
+    let carouselRef=useRef(null)
     const handleToggle=(taskId)=>{
         onToggleChange(taskId)
     }
@@ -10,7 +15,36 @@ function TaskRender({tasks ,onToggleChange,getSelectedTask,getId}) {
     const handleDelete=(taskId)=>{
         getId(taskId)
     }
+    
+// useEffect(() => {
+//   const interval = setInterval(() => {
+//     handleRightClick();
+//   }, 2000);
+
+//   return () => clearInterval(interval);
+// });
+
+ function handleRightClick() {
+    let newIndex = currentIndex + 1
+  if(newIndex >= tasks.length) {
+        newIndex = 0
+    }
+    setCurrentIndex(newIndex)
+    carouselRef.current.scrollLeft = newIndex * itemWidth;
+  }
+
+function handleLeftClick() {
+    let newIndex = currentIndex - 1
+  if(newIndex < 0) {
+        newIndex = tasks.length - 1
+    }
+    setCurrentIndex(newIndex)
+    carouselRef.current.scrollLeft = newIndex * itemWidth;
+  }
+
   return (
+    <>
+    <div className={styles.carousel} ref={carouselRef}>
     <ul>
 {tasks.map(task=>(
     <li key={task.id}>
@@ -24,6 +58,16 @@ function TaskRender({tasks ,onToggleChange,getSelectedTask,getId}) {
 )
 )}
     </ul>
+    </div>
+    <div>
+    <button className="leftBtn" onClick={handleLeftClick}>
+        left
+    </button>
+    <button className="rightBtn" onClick={handleRightClick}>
+        right
+    </button>
+    </div>
+    </>
   )
 }
 
